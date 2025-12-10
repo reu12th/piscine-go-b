0ca0c37acb291eda2f2ed0c51776bfe8b132f0ae
Reuben Abdul <reu12th@gmail.com>
2025-12-10 17:23:50 +0100

    q4 t2

---
+++ recursivefactorial.go
@@ -0,0 +1,13 @@
+package piscine
+
+func RecursiveFactorial(nb int) int {
+	if nb < 0 || nb > 20 {
+		return 0
+	}
+	if nb == 0 || nb == 1 {
+		return 1
+	}
+	factorial := nb * RecursiveFactorial(nb-1)
+
+	return factorial
+}
