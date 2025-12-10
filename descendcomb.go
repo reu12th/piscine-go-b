f99b94eb556c47329d6841f64a4427379705340d
Reuben Abdul <reu12th@gmail.com>
2025-12-10 17:24:24 +0100

    descendcomb

---
+++ descendcomb.go
@@ -0,0 +1,37 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func DescendComb() {
+	for i := '9'; i >= '0'; i-- {
+		for j := '9'; j >= '0'; j-- {
+			for x := '9'; x >= '0'; x-- {
+				for y := '9'; y >= '0'; y-- {
+					o1 := int(j) * 1
+					o2 := int(y) * 1
+					t1 := int(i) * 10
+					t2 := int(x) * 10
+
+					if o1+t1 <= o2+t2 {
+						continue
+					}
+					printCombNum(i)
+					printCombNum(j)
+					printCombNum(' ')
+					printCombNum(x)
+					printCombNum(y)
+
+					if i != '0' || j != '1' || x != '0' || y != '0' {
+						printCombNum(',')
+						printCombNum(' ')
+					}
+
+				}
+			}
+		}
+	}
+}
+
+func printCombNum(r rune) {
+	z01.PrintRune(r)
+}
