1143ebdcdcaffc9081f8230788bea32d0b11ad58
Reuben Abdul <reu12th@gmail.com>
2025-12-10 17:23:50 +0100

    q11 t1

---
+++ listpushback.go
@@ -0,0 +1,25 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+	n := &NodeL{Data: data}
+
+	if l.Head == nil {
+		l.Head = n
+		return
+	}
+	currentnode := l.Head
+	for currentnode.Next != nil {
+		currentnode = currentnode.Next
+	}
+	currentnode.Next = n
+}
