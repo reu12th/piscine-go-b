94c8738e83a15ce04cfdc33706923531c167691c
Reuben Abdul <reu12th@gmail.com>
2025-12-10 17:23:50 +0100

    q4 t5

---
+++ fibonacci.go
@@ -0,0 +1,18 @@
+package piscine
+
+func Fibonacci(index int) int {
+	if index < 0 {
+		return -1
+	}
+	if index == 0 {
+		return 0
+	}
+
+	if index == 1 {
+		return 1
+	}
+
+	result := Fibonacci(index-1) + Fibonacci(index-2)
+
+	return result
+}
