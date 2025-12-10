4404b60047fa34121b6083dba612f20814d59dae
Reuben Abdul <reu12th@gmail.com>
2025-12-10 17:23:50 +0100

    q2 t6

---
+++ printcomb2.go
@@ -0,0 +1,29 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	for a := '0'; a <= '9'; a++ {
+		for b := '0'; b <= '9'; b++ {
+			for c := '0'; c <= '9'; c++ {
+				dStart := '0'
+				if c == a {
+					dStart = b + 1
+				}
+				for d := dStart; d <= '9'; d++ {
+					z01.PrintRune(a)
+					z01.PrintRune(b)
+					z01.PrintRune(' ')
+					z01.PrintRune(c)
+					z01.PrintRune(d)
+
+					if a != '9' || b != '8' || c != '9' || d != '9' {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+			}
+
+		}
+	}
+}
