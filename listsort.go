88ad36f65c6463c918b189bec4282e358b089490
Rubenstein <reu12th@gmail.com>
2025-12-12 04:48:54 +0100

    listsort

---
+++ listsort.go
@@ -0,0 +1,32 @@
+package piscine
+
+type NodeI struct {
+	Data int
+	Next *NodeI
+}
+
+func ListSort(l *NodeI) *NodeI {
+	cmpt := 0
+	var first *NodeI
+
+	if l == nil || l.Next == nil {
+		return l
+	}
+
+	for l != nil {
+		next := l.Next
+		if cmpt == 0 {
+			first = l
+			cmpt++
+		}
+
+		for next != nil {
+			if l.Data > next.Data {
+				l.Data, next.Data = next.Data, l.Data
+			}
+			next = next.Next
+		}
+		l = l.Next
+	}
+	return first
+}
